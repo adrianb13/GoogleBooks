c34af2b58667a97cac1638458fa1d668ef19d49e
Adrian Brinoes <Coding4Adrian@yahoo.com>
2019-04-24 17:56:08 -0700

    api calls

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
 import "./App.css";
-import NavBar from "./NavBar";
-import Header from "./Header";
-import Books from "./Books";
+//import NavBar from "./components/NavBar";
+//import Header from "./components/Header";
+import Books from "./components/Books";
 import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
 function App() {
   return (
     <Router>
-      <NavBar />
-      <Header />
+{/*       <NavBar />
+      <Header /> */}
       <Switch>
         <Route exact path="/" component={Books} />
         <Route exact path="/search" component={Books} />
--- client/src/components/Books/index.js
+++ client/src/components/Books/index.js
@@ -1,13 +1,43 @@
 import React from "react";
-import Search from "../Search";
-import Saved from "../Saved";
+//import Search from "../Search";
+//import Saved from "../Saved";
+import API from "../../utils/API";
 
 class Books extends React.Component {
+  state = {
+    books: [],
+    title: "",
+    author: "",
+    description: "",
+    image: "",
+    link: "",
+    result: []
+  };
+
+  componentDidMount() {
+    this.searchBooks("game of thrones");
+  }
+
+  searchBooks = query => {
+    API.search(query)
+      .then(res => {
+        this.setState({ result: res.data.items })
+        console.log(this.state.result)
+        console.log(this.state.result[0].volumeInfo)
+        console.log("Book Title:" + this.state.result[0].volumeInfo.title)
+        console.log("Author(s):" + this.state.result[0].volumeInfo.authors)
+        console.log("Description:" + this.state.result[0].volumeInfo.description)
+        console.log("Image Link:" + this.state.result[0].volumeInfo.imageLinks.thumbnail)
+        console.log("Book Link:" + this.state.result[0].volumeInfo.canonicalVolumeLink)})      
+      .catch(err => console.log(err));
+  }
+
   render() {
     return (
       <div>
-        <Search />
-        <Saved />
+{/*         {this.state.result.map(book => (
+          <div>{this.state.result.volumeInfo.title}</div>
+        ))} */}
       </div>
     );
   }
