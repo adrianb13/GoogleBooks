16fa10b941ddabe76dbd24a25868babdc6cf2b37
Adrian Brinoes <Coding4Adrian@yahoo.com>
2019-04-28 15:12:50 -0700

    Updates

---
+++ precache-manifest.27e422e04d3492d99badf577208b6fa0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7230a12f88474f786294072a702ed749",
+    "url": "/GoogleBooks/index.html"
+  },
+  {
+    "revision": "3b18aa6a14fca8e87878",
+    "url": "/GoogleBooks/static/css/main.9a318d24.chunk.css"
+  },
+  {
+    "revision": "58a4c2c3d2ed6bf96de4",
+    "url": "/GoogleBooks/static/js/2.22794989.chunk.js"
+  },
+  {
+    "revision": "3b18aa6a14fca8e87878",
+    "url": "/GoogleBooks/static/js/main.2d7b409b.chunk.js"
+  },
+  {
+    "revision": "4855e103f313aba37612",
+    "url": "/GoogleBooks/static/js/runtime~main.6d6b3e7d.js"
+  }
+]);
